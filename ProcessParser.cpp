5a7008ab4a53d5e8d0dad7c36fd35853bd37f23a
igor6629 <nechepurenko6629@gmail.com>
2023-08-10 22:42:38 +0200

    defining class methods

--- ProcessParser.cpp
+++ ProcessParser.cpp
@@ -16,7 +16,9 @@
 #include <dirent.h>
 #include <time.h>
 #include <unistd.h>
+#include <filesystem>
 #include <constants.h>
+#include <util.h>
 
 using namespace std;
 
@@ -39,3 +41,55 @@ public:
     static string getOsName();
     static string printCpuStats(vector<string> values1, vector<string>values2);
 };
+
+// getCmd returns Cmd info of current pid
+string ProcessParser::getCmd(string pid)
+{
+    string line;
+    ifstream stream = Util::getStream((Path::basePath() + pid + Path::cmdPath()));
+    getline(stream, line);
+    
+    return line;
+}
+
+// getPidList returns list of all pids 
+vector<string> ProcessParser::getPidList()
+{
+    vector<string> pidList; 
+
+    for (const auto & entry : filesystem::directory_iterator(Path::basePath())) 
+    {
+        string current = entry.path();
+        current = current.substr(6, current.size());
+        
+        if (current[0] >= '0' && current[0] <= '9')
+            pidList.push_back(current);
+    }
+
+    return pidList;
+}
+
+// getVmSize returns VM size of current pid
+string ProcessParser::getVmSize(string pid)
+{
+    string name = "VmData";
+    string line;
+    float size;
+
+    ifstream stream = Util::getStream(Path::basePath() + pid + Path::statusPath());
+
+    while (getline(stream, line))
+    {
+        if (line.compare(0, name.size(), name) == 0)
+        {
+            istringstream buf(line);
+            istream_iterator<string> beg(buf), end;
+            vector<string> values(beg, end);
+
+            size = (stof(values[1]) / float(1024));
+            break;
+        }
+    }
+
+    return to_string(size);
+}
