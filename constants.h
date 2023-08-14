e7e4a2be8443d005945da9d4a9d40cd3e4fb5a48
igor6629 <nechepurenko6629@gmail.com>
2023-08-14 18:11:35 +0200

    Adding new enum

--- constants.h
+++ constants.h
@@ -4,6 +4,20 @@
 
 using namespace std;
 
+enum CPUStates 
+{
+  S_USER = 1,
+  S_NICE,
+  S_SYSTEM,
+  S_IDLE,
+  S_IOWAIT,
+  S_IRQ,
+  S_SOFTIRQ,
+  S_STEAL,
+  S_GUEST,
+  S_GUEST_NICE
+};
+
 class Path
 {
 public:
